2763cbbbdaa97a67c5dcc62266f772b1f6a2d5ff
IvanProdaiko94 <prodaiko94@gmail.com>
2019-06-11 11:07:39 +0300

    add new call

--- proxy/server.go
+++ proxy/server.go
@@ -34,9 +34,13 @@ type server struct {
 }
 
 func (s *server) Start(ctx context.Context) error {
+	var mu sync.Mutex
 	for _, client := range s.clients {
 		if err := client.Deal(); err != nil {
-			fmt.Errorf("failed to connect to client: %s", err.Error())
+			mu.Lock()
+			_, _ = os.Stderr.Write([]byte(err.Error()))
+			_, _ = os.Stderr.Write([]byte("\n"))
+			mu.Unlock()
 		}
 	}
 	if err := s.srv.ListenAndServe(); err != nil {
@@ -52,6 +56,83 @@ func (s *server) Stop() error {
 	return s.srv.Shutdown(context.Background())
 }
 
+func (s *server) GetLog(w http.ResponseWriter, r *http.Request) {
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	ctx, _ := context.WithTimeout(context.Background(), time.Second*5)
+
+	for _, cl := range s.clients {
+		wg.Add(1)
+		go func(client rpc.Client) {
+			defer wg.Done()
+			log, err := client.GetLog(ctx, &empty.Empty{})
+
+			mu.Lock()
+			defer mu.Unlock()
+			if err != nil {
+				_, _ = w.Write([]byte(err.Error()))
+				_, _ = w.Write([]byte("\n"))
+				return
+			}
+			data, err := json.Marshal(&log)
+			if err != nil {
+				_, _ = w.Write([]byte(err.Error()))
+				_, _ = w.Write([]byte("\n"))
+				return
+			}
+			_, _ = w.Write(data)
+			_, _ = w.Write([]byte("\n"))
+		}(cl)
+	}
+	wg.Wait()
+}
+
+func (s *server) NewEntry(w http.ResponseWriter, r *http.Request) {
+	var cmd schema.Command
+
+	body, _ := ioutil.ReadAll(r.Body)
+	err := json.Unmarshal(body, &cmd)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	success := false
+
+	ctx, _ := context.WithTimeout(context.Background(), time.Second*2)
+
+	for !success {
+		if s.leader == -1 {
+			s.leader = randRange(0, len(env.Cluster))
+		}
+
+		client := s.clients[s.leader]
+		fmt.Printf("\n%#v\n", client)
+		resp, err := client.NewEntry(ctx, &cmd)
+		if err == context.DeadlineExceeded {
+			return
+		} else if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			continue
+		}
+		fmt.Printf("\n expected leader: %d. actual: %d\n", s.leader, resp.Leader)
+		fmt.Printf("\n leader id: %d. Result: %#v\n", s.leader, resp)
+
+		if int(resp.Leader) != s.leader {
+			s.leader = int(resp.Leader)
+		}
+
+		if resp.Success {
+			success = resp.Success
+			_, err = w.Write([]byte(fmt.Sprintf("%t", resp.Success)))
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+	}
+}
+
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost && r.Method != http.MethodGet {
 		http.Error(w, "not allowed", http.StatusMethodNotAllowed)
@@ -60,74 +141,11 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	if r.Method == http.MethodGet {
-		var wg sync.WaitGroup
-		var mu sync.Mutex
-		ctx, _ := context.WithTimeout(context.Background(), time.Second*5)
-
-		for _, client := range s.clients {
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				log, err := client.GetLog(ctx, &empty.Empty{})
-
-				mu.Lock()
-				defer mu.Unlock()
-				fmt.Println(log)
-				if err != nil {
-					_, _ = os.Stderr.Write([]byte(err.Error()))
-					_, _ = os.Stderr.Write([]byte("\n"))
-					return
-				}
-				_, _ = w.Write([]byte(fmt.Sprintf("%#v\n", log)))
-			}()
-		}
-		wg.Wait()
+		s.GetLog(w, r)
 	}
 
 	if r.Method == http.MethodPost {
-		var cmd schema.Command
-
-		body, _ := ioutil.ReadAll(r.Body)
-		err := json.Unmarshal(body, &cmd)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		success := false
-
-		ctx, _ := context.WithTimeout(context.Background(), time.Second*2)
-
-		for !success {
-			if s.leader == -1 {
-				s.leader = randRange(0, len(env.Cluster))
-			}
-
-			client := s.clients[s.leader]
-			fmt.Printf("\n%#v\n", client)
-			resp, err := client.NewEntry(ctx, &cmd)
-			if err == context.DeadlineExceeded {
-				return
-			} else if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				continue
-			}
-			fmt.Printf("\n expected leader: %d. actual: %d\n", s.leader, resp.Leader)
-			fmt.Printf("\n leader id: %d. Result: %#v\n", s.leader, resp)
-
-			if int(resp.Leader) != s.leader {
-				s.leader = int(resp.Leader)
-			}
-
-			if resp.Success {
-				success = resp.Success
-				_, err = w.Write([]byte(fmt.Sprintf("%t", resp.Success)))
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-					return
-				}
-			}
-		}
+		s.NewEntry(w, r)
 	}
 }
 
